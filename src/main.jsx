3d5b1b5c6cfcd1357f2fb965aa25c4e88e756849
Biruk <birukb1152@gmail.com>
2026-03-18 21:26:28 +0300

    fix github pages issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { DarkModeProvider } from "./hooks/DarkModeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <DarkModeProvider>
         <App />
       </DarkModeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/education_admin_dashboard",
+  base: "/education_admin_dashboard/",
 });
